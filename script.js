c2f2212900f1887a49da36d69111e30e08312021
Mabhuka <brandonmabhuka@gmail.com>
2026-01-18 11:00:58 +0200

    Programming Languages Projects Section
    Hide and Unhide

--- script.js
+++ script.js
@@ -9,6 +9,10 @@ function toggleMenu() {
   let projectSection = document.querySelector(".projects-demo")
   let projectSection2 = document.querySelector(".projects-demo2")
   let projectSection3 = document.querySelector(".projects-demo3")
+
+  let projectSection4 = document.querySelector(".projects-demo4")
+  let projectSection5 = document.querySelector(".projects-demo5")
+  let projectSection6 = document.querySelector(".projects-demo6")
   
   function showHideProjects(){
     isShow = true;
@@ -28,4 +32,25 @@ function toggleMenu() {
     projectSection3.classList.toggle("hide");
     projectSection.classList.toggle("hide",isShow);
     projectSection2.classList.toggle("hide",isShow);
+  }
+
+  function showHideProjects4(){
+    isShow = true;
+    projectSection4.classList.toggle("hide");
+    projectSection5.classList.toggle("hide",isShow);
+    projectSection6.classList.toggle("hide",isShow);
+  }
+
+  function showHideProjects5(){
+    isShow = true;
+    projectSection5.classList.toggle("hide");
+    projectSection4.classList.toggle("hide",isShow);
+    projectSection6.classList.toggle("hide",isShow);
+  }
+
+  function showHideProjects6(){
+    isShow = true;
+    projectSection6.classList.toggle("hide");
+    projectSection4.classList.toggle("hide",isShow);
+    projectSection5.classList.toggle("hide",isShow);
   }
